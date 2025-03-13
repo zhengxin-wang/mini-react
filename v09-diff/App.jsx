5607c04ed53f6a804b2b9c09b385e4d266870f17
Jackson Wang <Jackson_Wang@epam.com>
2025-03-13 17:52:57 +0800

    day05 part4: initial setup

--- v09-diff/App.jsx
+++ v09-diff/App.jsx
@@ -1,30 +1,49 @@
 import React from "./core/React.js"
 
-let showBar = false
-function Counter() {
-  const bar = <div>bar</div>
-  function handleShowBar() {
-    showBar = !showBar
+let countFoo1 = 1
+function Foo() {
+  console.log("Foo return ")
+  function handleClick() {
+    countFoo1++
     React.update()
   }
   return (
     <div>
-      counter
-      {showBar && bar}
-      <button onClick={handleShowBar}>showBar</button>
+      <h1>Foo : {countFoo1}</h1>
+      <button onClick={handleClick}>click</button>
     </div>
   )
 }
-
-console.log(Counter());
-
-function App() {
+let countBar = 1
+function Bar() {
+  console.log("Bar return ")
+  function handleClick() {
+    countBar++
+    React.update()
+  }
   return (
     <div>
-      mini-react
-      <Counter></Counter>
+      <h1>Bar : {countBar}</h1>
+      <button onClick={handleClick}>click</button>
+    </div>
+  )
+}
+let countApp = 1
+function App() {
+  console.log("App return ")
+  function handleClick() {
+    countApp++
+    React.update()
+  }
+  return (
+    <div>
+      <h1>App : {countApp}</h1>
+      <button onClick={handleClick}>click</button>
+      <Foo></Foo>
+      <Bar></Bar>
     </div>
   )
 }
 
 export default App
+
